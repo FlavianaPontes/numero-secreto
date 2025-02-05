7a9d0af4a39dc4a157dab01ff407c28a2b5ef8d2
Flávia Pontes <flavianap.2012@gmail.com>
2025-02-05 17:58:52 -0400

    projeto inicial

---
+++ app.js
@@ -0,0 +1,83 @@
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML = 'jogo secreto';
+
+//let pararagrafo = document.querySelector('p');
+//pararagrafo.innerHTML = 'escolha um numero de 1 a 10';
+
+let listaDeNumerosSorteados = [ ];
+let numeroLimite = 10;
+let numerosecreto = gerarNumeroAleatorio();
+
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto){   //parametros
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela(`h1`, 'jogo secreto');
+    exibirTextoNaTela(`p`, 'escolha um numero de 1 a 10');
+}
+
+exibirMensagemInicial();
+
+function verificarChute(){
+    let chute = document.querySelector(`input`).value;
+    if (chute == numerosecreto) {
+        exibirTextoNaTela('h1', 'acertou');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `descobriu com ${tentativas} ${palavraTentativa}`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+
+    } else {
+        if(chute > numerosecreto){
+            exibirTextoNaTela('p', `o numero secreto é menor que ${chute}`);
+        }
+        else{
+            exibirTextoNaTela('p', `o numero secreto é maior que ${chute}`);
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+
+function gerarNumeroAleatorio() {
+   let  numeroEscolhido = parseInt(Math.random()*numeroLimite+1);
+   let quantDeElementosNaLista = listaDeNumerosSorteados.length;
+   //verificação
+   if (quantDeElementosNaLista == numeroLimite) {
+    listaDeNumerosSorteados = []
+   } 
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) { // verifica se o elemento ta na lista ... include
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido) //inserir elementos na lita
+        return numeroEscolhido
+    }
+}  //ele vai retornar algo nessa pagina
+
+
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo(){
+    numerosecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
